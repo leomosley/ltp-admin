ff6b90544e46aea596dd1541b004758a9cbee054
leomosley <leo.mosley2@gmail.com>
2023-12-14 18:55:02 +0000

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import styles from './page.module.css';
 
+/* 
+  Framer motion hover affect - typehero
+  auth context
+  database operations
+  dark mode
+  
+*/
+
+
+
 export default function Home() {
+
   return (
-    <div>Hello</div>
+    <div>hello</div>
   )
 }
